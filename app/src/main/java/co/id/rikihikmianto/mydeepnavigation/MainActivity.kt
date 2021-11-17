0687c423eeaa62b7aa74eade8b72ebecfdcc9e8a
RikiHikmianto <dayatketangga95@gmail.com>
2021-11-17 10:38:00 +0700

    first commit

---
+++ app/src/main/java/co/id/rikihikmianto/mydeepnavigation/MainActivity.kt
@@ -0,0 +1,88 @@
+package co.id.rikihikmianto.mydeepnavigation
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import co.id.rikihikmianto.mydeepnavigation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnOpenDetail.setOnClickListener(this)
+
+        showNotification(
+            this,
+            getString(R.string.notification_title),
+            getString(R.string.notification_message),
+            110
+        )
+    }
+
+    override fun onClick(v: View?) {
+        if (v!!.id == R.id.btnOpenDetail) {
+            val detailIntent = Intent(this, DetailActivity::class.java)
+            detailIntent.putExtra(DetailActivity.EXTRA_TITLE, getString(R.string.detail_title))
+            detailIntent.putExtra(DetailActivity.EXTRA_MESSAGE, getString(R.string.detail_message))
+            startActivity(detailIntent)
+        }
+    }
+
+    private fun showNotification(context: Context, title: String, message: String, notifId: Int) {
+        val CHANNEL_ID = "Channel_1"
+        val CHANNEL_NAME = "Navigation channel"
+
+        val notifDetailntent = Intent(this, DetailActivity::class.java)
+        notifDetailntent.putExtra(DetailActivity.EXTRA_TITLE, title)
+        notifDetailntent.putExtra(DetailActivity.EXTRA_MESSAGE, message)
+
+        val pendingIntent = TaskStackBuilder.create(this)
+            .addParentStack(DetailActivity::class.java)
+            .addNextIntent(notifDetailntent)
+            .getPendingIntent(110, PendingIntent.FLAG_MUTABLE)
+
+        val notificationManagerCompat =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val builder = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setContentTitle(title)
+            .setSmallIcon(R.drawable.ic_baseline_email_24)
+            .setContentText(message)
+            .setColor(ContextCompat.getColor(context, android.R.color.black))
+            .setVibrate(longArrayOf(1000, 1000, 1000, 1000, 1000))
+            .setSound(alarmSound)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+
+            channel.enableVibration(true)
+            channel.vibrationPattern = longArrayOf(1000, 1000, 1000, 1000, 1000)
+
+            builder.setChannelId(CHANNEL_ID)
+            notificationManagerCompat.createNotificationChannel(channel)
+        }
+        val notification = builder.build()
+        notificationManagerCompat.notify(notifId, notification)
+
+    }
+}
